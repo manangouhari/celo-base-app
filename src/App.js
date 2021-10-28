db8def76477d03d193b5e3213ec431ec73133b2b
Manan Gouhari <gouhari21@hotmail.com>
2021-10-28 10:28:03 +0200

    base template to connect to wallet.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, Box, Button, Text } from "@chakra-ui/react";
+import { useContractKit } from "@celo-tools/use-contractkit";
+import "@celo-tools/use-contractkit/lib/styles.css";
 
 function App() {
+  const { connect, destroy, address } = useContractKit();
+
+  function connectWallet() {
+    try {
+      connect();
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  function disconnectWallet() {
+    try {
+      destroy();
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container m={10}>
+      <Box>
+        <Button colorScheme={"green"} onClick={connectWallet}>
+          Connect Wallet
+        </Button>
+        <Button colorScheme={"yellow"} onClick={disconnectWallet} ml={4}>
+          Disconnect Wallet
+        </Button>
+      </Box>
+      <Box mt={10}>
+        <Text>{address}</Text>
+      </Box>
+    </Container>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+import { ChakraProvider } from "@chakra-ui/react";
+import { ContractKitProvider, Alfajores } from "@celo-tools/use-contractkit";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <ContractKitProvider
+        networks={[Alfajores]}
+        dapp={{
+          name: "NFT Market",
+          description: "_",
+          url: "https://example.com",
+        }}
+      >
+        <App />
+      </ContractKitProvider>
+    </ChakraProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
